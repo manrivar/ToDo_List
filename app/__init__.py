7d333714739d16d5e164102241a3b555d18d7990
manrivar <manrivargueta@gmail.com>
2024-03-09 09:02:46 -0600

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+
+    app = Flask(__name__)
+
+    # Configuracion del proyecto
+    app.config.from_mapping(
+        DEBUG = True,
+        SECRET_KEY = 'dev',
+        SQLALCHEMY_DATABASE_URI = "sqlite:///todolist.db"
+    )
+
+    #db inicializado para poderlo usar
+    db.init_app(app)
+
+    #Registro de Blueprints
+    from . import todo
+    app.register_blueprint(todo.bp)
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    #Ruta principal
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    with app.app_context():
+        db.create_all()
+    
+    return app
